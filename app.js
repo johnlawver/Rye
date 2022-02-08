5c4b719e161eb9f97146db53e949147f21f6b063
John Lawver <johnlawver@gmail.com>
2022-02-08 12:33:53 -0800

    1: added worker manifest and assets

--- app.js
+++ app.js
@@ -22,4 +22,11 @@ var firstPaint = () => {
   container.className = pickColor(colorsArray,1);
 };
 
-
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+      navigator.serviceWorker
+        .register("/serviceWorker.js")
+        .then(res => console.log("service worker registered"))
+        .catch(err => console.log("service worker not registered", err))
+    })
+  }
