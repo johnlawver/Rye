9eb53aee6721c0568b051fcb04973b8345ce1f41
John Lawver <johnlawver@gmail.com>
2022-02-08 11:12:47 -0800

    initialize app

---
+++ app.js
@@ -0,0 +1,25 @@
+let colorsArray = ['red', 'blue', 'green', 'yellow', 'purple', 'pink', 'orange']
+
+var pickColor = (arr, n) => {
+    var result = new Array(n),
+        len = arr.length,
+        taken = new Array(len);
+    if (n > len)
+        throw new RangeError("getRandom: more elements taken than available");
+    while (n--) {
+        var x = Math.floor(Math.random() * len);
+        result[n] = arr[x in taken ? taken[x] : x];
+        taken[x] = --len in taken ? taken[len] : len;
+    }
+    return result;
+};
+
+var container = document.getElementById("container").className = pickColor(colorsArray,1);
+
+var firstPaint = () => {
+  var container = document.getElementById("container");
+  console.log("it worked");
+  container.className = pickColor(colorsArray,1);
+};
+
+
